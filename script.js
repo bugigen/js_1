df6f033f2d33a68ec34c49798c2e4a56c41507ac
elgrenka <bugigen@yandex.ru>
2022-08-25 22:40:19 +0400

    ready code

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 let content = document.querySelector("#content");
+let product = document.querySelector("#product");
+let btn = document.querySelector("#btn");
 
-document.addEventListener("DOMContentLoaded", getJson);
+const url = "https://run.mocky.io/v3/adf501fe-5592-4766-8a10-0bc24f328807";
 
-var getJson = () => {
-  products = {
-    id: 1
+const getJson = async () => {
+  try {
+    const response = await fetch(url);
+    if (response.ok === "false") {
+      const message = `An error has occured: ${response.status}`;
+      throw new Error(message);
+    }
+    const answer = await response.json();
+    console.log(answer);
+    // const answerText = await response.text();
+    content.textContent = JSON.stringify(answer, null, 2);
+    product.textContent = answer.Cases[0].name;
+  } catch (e) {
+    console.log(e);
   }
-
 }
 
-console.log(getJson)
+// getJson().catch(error => error.message);
+
+btn.addEventListener("click", getJson);
+
+
